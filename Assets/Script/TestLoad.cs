f682c3706735d30f0d4dd5f0ca4b09b0ed5f83b1
tinyshan <tinyshan@tencent.com>
2021-11-19 20:08:24 +0800

    老电话模拟程序

---
+++ Assets/Script/TestLoad.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+public class TestLoad : MonoBehaviour
+{
+    public GameObject TestUI;
+    public Button _btnLoad;
+    public Transform _Canvas;
+
+
+
+    private void Awake()
+    {
+        this._btnLoad.onClick.AddListener(LoadShowPanel);
+    }
+
+    private void LoadShowPanel()
+    {
+        /*   if (this._view == null)
+        {
+            GameObject obj = Resources.Load<GameObject>("PhoneTimeLineUi");
+            GameObject ui = GameObject.Instantiate(obj, _Canvas, false);
+            var rectForm = ui.GetComponent<RectTransform>();
+            var offmini = rectForm.offsetMin;
+            var offmax = rectForm.offsetMax;
+            rectForm.offsetMax = Vector2.zero;
+            rectForm.offsetMin = Vector2.zero;
+            rectForm.localScale = Vector3.one;
+            rectForm.localPosition = Vector3.zero;
+            this._view = ui.GetComponent<PhoneTimeLineUi>();
+        }
+        this._view.transform.SetAsLastSibling();
+        this._view.gameObject.SetActive(true);
+        this.TestUI.SetActive(false);
+        this._view.Init();
+        */
+    }
+
+}//end class
---
+++ Assets/Script/UIDragHandler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+
+
+public class UIDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler
+{
+    public UnityAction<PointerEventData, object> _downCall;
+    public UnityAction<PointerEventData, object> _beginCall;
+    public UnityAction<PointerEventData, object> _dragCall;
+    public UnityAction<PointerEventData, object> _endCall;
+    private object mParam;
+    public void SetEnable(bool isEnable)
+    {
+        this.enabled = isEnable;
+    }
+
+    public void SetParam(object param)
+    {
+        this.mParam = param;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (this._downCall != null)
+            this._downCall.Invoke(eventData, mParam);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (this._beginCall != null)
+            this._beginCall.Invoke(eventData, mParam);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (this._dragCall != null)
+            this._dragCall.Invoke(eventData, mParam);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (this._endCall != null)
+            this._endCall.Invoke(eventData, mParam);
+    }
+}
+
